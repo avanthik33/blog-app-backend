e1ae7d10807f9d3c11be568721cd9231a3314b2e
avanthik33 <avanthik33@gmail.com>
2024-02-09 11:42:20 +0530

    .\.git\

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -1,11 +1,27 @@
 const express=require("express")
 const usersModel=require("../models/usersModel")
 const router=express.Router()
+const bcrypt=require("bcryptjs")
+
+const hashFunction=async(password)=>{
+  const salt=await bcrypt.genSalt(10)
+  return bcrypt.hash(password,salt)
+}
+
 
 router.post("/signup",async(req,res)=>{
-  let data=req.body
-  let modelObj1=new usersModel(data)
-  await modelObj1.save()
+  let {data} = {"data":req.body}
+  let password=data.password
+
+
+  hashFunction(password).then((hashedPassword)=>{
+    data.password=hashedPassword
+    console.log(data)
+    let userObj=new usersModel(data)
+     userObj.save()
+   
+  })
+  
   res.json({status:"success"})
 })
 
